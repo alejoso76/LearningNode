7b5da233b6d823c686f34cb662d59a0dfa98bfdf
alejoso76 <alejandroosoriotrujillo@gmail.com>
2020-03-06 12:01:06 -0500

    Comments descriptivos

--- Introduction/myfirst.js
+++ Introduction/myfirst.js
@@ -1,7 +1,9 @@
 var http = require('http'); //Importa el modulo
 
 //Crea un objeto de servidor
+//*req significa request del cliente
 http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'}); //200 significa todo OK y objeto conteniendo headers de respuesta
     res.write('Hello world'); //Escrba una respuesta al cliente
     res.end(); //FInaliza la respuesta
 }).listen(8080); //EL objeto escucha en el puerto 8080
